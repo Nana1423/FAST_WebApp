a6b0be03ee463443ad9c256e960f6c52b99e8f9a
Nana1423 <emanuel.leite10@outlook.com>
2024-02-19 19:03:38 -0600

    Adding FAST colors to Tailwind Theme

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <>
-            <h1>Hello Luswi!</h1>
+            <h1 className="bg-FAST-Orange text-lg">Hello Luswi!</h1>
         </>
     )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "FAST-Orange": "#FF7622",
+      "FAST-Grey": "#98A8B8",
+      "FAST-DarkGrey": "#6B6E82",
+      "FAST-DarkBlue": "#121223",
+      "FAST-WhiteCream": "#F0F5FA"
+    },
     extend: {},
   },
   plugins: [],
