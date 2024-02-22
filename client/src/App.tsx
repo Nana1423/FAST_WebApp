202d8721e02c4679b32b056afe17978ac44ae1ae
Emanuel Leite <emanuel.leite10@outlook.com>
2024-02-22 14:34:00 -0600

    Adding Login Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Login from "./pages/Login";
 
 function App() {
     return (
         <Routes>
 			<Route path="/" element={<Header />} />
-			<Route path="/admin" element={<Footer />} />
+			<Route path="/Login" element={<Login />} />
 		</Routes>
     )
 }
